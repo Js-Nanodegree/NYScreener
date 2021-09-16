c9827d01f1111e92367ad73a42130958f5e0ffa9
js-nanodegree <sy@zen.car>
2021-09-16 11:25:31 +0300

    fix screen

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,6 @@
 module.exports = {
-  apps: [{
-    name: "app1",
-    script: "npm run dev",
-    instances: "max",
-    exec_mode: "cluster",
-    env_production: {
-      NODE_ENV: "production",
-      PORT:3000,
-      HOSTNAME:"0.0.0.0",
-      "APOLLO_KEY": "service:Js-Nanodegree-3304:7v9zYQ7V587AP1oF_0eT8w",
-      "APOLLO_GRAPH_ID": "Js-Nanodegree-3304",
-      "APOLLO_GRAPH_VARIANT": "current",
-      "APOLLO_SCHEMA_REPORTING": "true",
-    },
-    env_development: {
-      NODE_ENV: "development",
-      PORT:3000,
-      HOSTNAME:"0.0.0.0",
-      "APOLLO_KEY": "service:Js-Nanodegree-3304:7v9zYQ7V587AP1oF_0eT8w",
-      "APOLLO_GRAPH_ID": "Js-Nanodegree-3304",
-      "APOLLO_GRAPH_VARIANT": "current",
-      "APOLLO_SCHEMA_REPORTING": "true",
-    }
+  apps : [{
+    name   : "app1",
+    script : "./index.js"
   }]
 }
