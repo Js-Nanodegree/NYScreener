a8f594fd5f18688e7c150acfd4126c1ebdb0cc9e
js-nanodegree <sy@zen.car>
2021-09-16 10:43:29 +0300

    fix port

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const server = new ApolloServer({
     schema,
 });
 
-server.listen().then(({ url }) => {
+server.listen(3000).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
 });
